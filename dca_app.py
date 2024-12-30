3fbdc613771239cceb55d691f35bf9d99f5e5d32
Jose Carlos Reyes <153877979+jcreyesh@users.noreply.github.com>
2024-12-30 16:20:49 -0600

    Update dca_app.py

--- dca_app.py
+++ dca_app.py
@@ -58,7 +58,7 @@ if file is not None:
     date_1 = data["fecha"].values[0]
     date_2 = data["fecha"].values[-1]
     start_date, end_date = sl.slider("Fechas:", min_value=date_1, max_value=date_2, value=(date_1, date_2), format="YYYY/MM/DD", 
-                                    step=timedelta(months=1)
+                                    step=timedelta(months=1))
     sl.write(start_date, end_date)
 
 
