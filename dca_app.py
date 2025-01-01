0989a9a52801d0f09fa46717d668500df3ce07f4
Jose Carlos Reyes <153877979+jcreyesh@users.noreply.github.com>
2025-01-01 17:42:45 -0600

    Update dca_app.py

--- dca_app.py
+++ dca_app.py
@@ -137,9 +137,9 @@ if file is not None:
     # fig.update_layout(yaxis2=dict(tickmode="sync"))
     # fig.update_yaxes(secondary_y=True, showgrid=False)
     
-    fig.update_yaxes(rangemode='tozero')
-    # fig.update_yaxes(range=[min(data["q"]), max(data["q"]) + 5], secondary_y=False)
-    # fig.update_yaxes(range=[0, max(df_dec["Np_arm"]) + 300], secondary_y=True)
+    # fig.update_yaxes(rangemode='tozero')
+    fig.update_yaxes(range=[-5, max(data["q"]) + 5], secondary_y=False)
+    fig.update_yaxes(range=[-5, max(df_dec["Np_arm"]) + 300], secondary_y=True)
     # fig.update_layout(width=1400, height=600)
     # fig.update_layout(title_x=0.4, title_y=0.85, plot_bgcolor="white")
     sl.plotly_chart(fig, key = 2)
