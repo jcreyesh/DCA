523f925179bb630d2edf445e7213be40e424a37c
Jose Carlos Reyes <153877979+jcreyesh@users.noreply.github.com>
2025-01-01 16:14:19 -0600

    Update dca_app.py

--- dca_app.py
+++ dca_app.py
@@ -134,7 +134,7 @@ if file is not None:
 
     fig.update_layout(yaxis2=dict(tickmode="sync"))
     fig.update_yaxes(secondary_y=True, showgrid=False)
-    # fig.update_layout(yaxis=dict(zeroline=False))
+    fig.update_layout(title_x=0.4, title_y=0.85, plot_bgcolor="white")
     fig.update_yaxes(rangemode='tozero')
     # fig.update_yaxes(range=[-1, max(data["q"]) + 5], secondary_y=False)
     # fig.update_yaxes(range=[-1, max(df_dec["Np_arm"]) + 300], secondary_y=True)
